a798c84c8369cae6467c3751bae231be963eca1e
Springhawk27 <sajjad0mahmud@gmail.com>
2023-12-01 14:54:31 +0600

    main route implemented

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,15 @@
 import express from 'express';
+import { TaskRoutes } from '../modules/task/task.routes';
 
 const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/tasks',
+    route: TaskRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
 
 export default router;
