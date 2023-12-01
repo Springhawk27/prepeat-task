aa656915ec9bb7e9c78002e304986a11ec473028
Springhawk27 <sajjad0mahmud@gmail.com>
2023-12-01 15:05:04 +0600

    validate middleware function implemented

--- src/app/modules/task/task.routes.ts
+++ src/app/modules/task/task.routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { TaskController } from './task.controller';
+import { TaskValidation } from './task.validation';
 
 const router = express.Router();
 
-router.post('/create-task', TaskController.createTask);
+router.post(
+  '/create-task',
+  validateRequest(TaskValidation.createTaskZodSchema),
+  TaskController.createTask,
+);
 router.get('/:id');
 
 router.patch('/:id');
